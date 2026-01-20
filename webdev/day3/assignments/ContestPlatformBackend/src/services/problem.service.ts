07f07df11e470c9a8b3db0e97e700da2b01bf063
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-21 00:26:20 +0530

    get dsa problem with visible test cases route

--- webdev/day3/assignments/ContestPlatformBackend/src/services/problem.service.ts
+++ webdev/day3/assignments/ContestPlatformBackend/src/services/problem.service.ts
@@ -4,13 +4,41 @@ import { AppError } from "../utils/AppError";
 export const getDsaProblemWithVisibleTestCasesService = async (
   problemId: number,
 ) => {
-  if (!problemId) throw new AppError("PROBLEM_ID not given", 404);
+  if (!problemId) {
+    throw new AppError("PROBLEM_ID_NOT_PROVIDED", 400);
+  }
 
-  const problem = await prisma.dsaQuestion.findFirst({
-    where: {
-      id: problemId,
+  const problem = await prisma.dsaQuestion.findUnique({
+    where: { id: problemId },
+    select: {
+      id: true,
+      contestId: true,
+      title: true,
+      description: true,
+      tags: true,
+      points: true,
+      timeLimit: true,
+      memoryLimit: true,
+
+      testCases: {
+        where: {
+          isHidden: false,
+        },
+        select: {
+          input: true,
+          expectedOutput: true,
+        },
+      },
     },
   });
 
-  return problem;
+  if (!problem) {
+    throw new AppError("PROBLEM_NOT_FOUND", 404);
+  }
+
+  return {
+    ...problem,
+    visibleTestCases: problem.testCases,
+    testCases: undefined,
+  };
 };

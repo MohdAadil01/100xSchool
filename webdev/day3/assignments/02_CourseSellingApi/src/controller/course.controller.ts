da75fb500b946ee0886999cd8f088403e099e7ed
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-24 16:45:15 +0530

    add course controller

--- webdev/day3/assignments/02_CourseSellingApi/src/controller/course.controller.ts
+++ webdev/day3/assignments/02_CourseSellingApi/src/controller/course.controller.ts
@@ -1,8 +1,64 @@
+import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/AsyncHandler";
 import { Request, Response } from "express";
+import {
+  createCourseInputSchema,
+  updateCourseInputSchema,
+} from "../validation/course.validation";
+import {
+  createCourseService,
+  deleteCourseService,
+  getCourseByIdService,
+  getCourseService,
+  updateCourseService,
+} from "../services/course.service";
 
 export const createCourse = asyncHandler(
   async (req: Request, res: Response) => {
-    return res.send("hi");
+    const parsedBody = createCourseInputSchema.parse(req.body);
+    const data = await createCourseService(
+      parsedBody,
+      req.user?.role!,
+      req.user?.id!,
+    );
+    return res.status(201).json(ApiResponse.success(data));
+  },
+);
+
+export const getCourses = asyncHandler(async (req: Request, res: Response) => {
+  const data = await getCourseService();
+  return res.status(200).json(ApiResponse.success(data));
+});
+
+export const getCourseByid = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { courseId } = req.params;
+    const data = await getCourseByIdService(String(courseId));
+
+    return res.status(200).json(ApiResponse.success(data));
+  },
+);
+export const updateCourse = asyncHandler(
+  async (req: Request, res: Response) => {
+    const courseId = req.params.courseId;
+    const parsedBody = updateCourseInputSchema.parse(req.body);
+    const data = await updateCourseService(
+      String(courseId),
+      parsedBody,
+      req.user?.role!,
+      req.user?.id!,
+    );
+    return res.status(200).json(ApiResponse.success(data));
+  },
+);
+export const deleteCourse = asyncHandler(
+  async (req: Request, res: Response) => {
+    const courseId = req.params.courseId;
+    const data = await deleteCourseService(
+      String(courseId),
+      req.user?.role!,
+      req.user?.id!,
+    );
+    return res.status(200).json(ApiResponse.success(data));
   },
 );

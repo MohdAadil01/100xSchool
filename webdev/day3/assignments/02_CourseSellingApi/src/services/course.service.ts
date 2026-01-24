a92efbee075b5537c9476802d89e023a4c81280f
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-24 16:36:51 +0530

    add course services

--- webdev/day3/assignments/02_CourseSellingApi/src/services/course.service.ts
+++ webdev/day3/assignments/02_CourseSellingApi/src/services/course.service.ts
@@ -1,6 +1,114 @@
-import { CreateCourseInputType } from "../validation/course.validation";
+import prisma from "../lib/prisma";
+import { AppError } from "../utils/AppError";
+import {
+  CourseIdInputType,
+  CreateCourseInputType,
+  UpdateCourseInputType,
+} from "../validation/course.validation";
 
 export const createCourseService = async (
   data: CreateCourseInputType,
   role: string,
-) => {};
+  instructorId: string,
+) => {
+  if (role.toLowerCase() != "instructor")
+    throw new AppError("UNAUTHORIZED", 403);
+  const { title, description, price } = data;
+  const existingCourse = await prisma.course.findFirst({
+    where: {
+      title,
+      description,
+      instructorId,
+    },
+  });
+  if (existingCourse) throw new AppError("Course already exists", 400);
+
+  const course = await prisma.course.create({
+    data: {
+      title,
+      description,
+      price,
+      instructorId,
+    },
+  });
+
+  return course;
+};
+
+export const getCourseService = async () => {
+  const courses = await prisma.course.findMany({});
+  if (courses.length === 0) throw new AppError("No Course found", 404);
+
+  return courses;
+};
+
+export const getCourseByIdService = async (courseId: string) => {
+  if (!courseId) throw new AppError("CourseID not given", 404);
+  const course = await prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+    select: {
+      lessions: true,
+    },
+  });
+  if (!course) throw new AppError("Course not found", 404);
+  return course;
+};
+
+export const updateCourseService = async (
+  courseId: string,
+  data: UpdateCourseInputType,
+  role: string,
+  instructorId: string,
+) => {
+  if (role.toLowerCase() != "instructor")
+    throw new AppError("Unauthorized", 403);
+  if (!courseId) throw new AppError("Course Id not given", 404);
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+  });
+  if (!course) throw new AppError("Course not found.", 404);
+
+  const isAuthorized = course.instructorId == instructorId;
+  if (!isAuthorized) throw new AppError("Unauthorized", 404);
+
+  const updatedCourse = await prisma.course.update({
+    where: {
+      id: courseId,
+    },
+    data,
+  });
+
+  return updatedCourse;
+};
+
+export const deleteCourseService = async (
+  courseId: string,
+  role: string,
+  instructorId: string,
+) => {
+  if (role.toLowerCase() != "instructor")
+    throw new AppError("Unauthorized", 403);
+  if (!courseId) throw new AppError("Course Id not given", 404);
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+  });
+  if (!course) throw new AppError("course not found", 404);
+  const isAuthorized = course?.instructorId == instructorId;
+  if (!isAuthorized) throw new AppError("Unauthorized", 403);
+
+  const deletedCourse = await prisma.course.delete({
+    where: {
+      id: courseId,
+    },
+  });
+
+  return deletedCourse;
+};
--- webdev/day3/assignments/02_CourseSellingApi/src/validation/course.validation.ts
+++ webdev/day3/assignments/02_CourseSellingApi/src/validation/course.validation.ts
@@ -4,13 +4,27 @@ export const createCourseInputSchema = z.object({
   title: z.string().min(4, "Title shoud be atlease 4 characters long."),
   description: z
     .string()
-    .min(10, "Description should be atleast 10 characters long").optional,
+    .min(10, "Description should be atleast 10 characters long")
+    .optional(),
   price: z.number().positive("Price can't be negative"),
 });
 
-export const purchaseCourseInputSchema = z.object({
+export const courseIdInputSchema = z.object({
   courseId: z.uuid(),
 });
 
+export const updateCourseInputSchema = z.object({
+  title: z
+    .string()
+    .min(4, "Title shoud be atlease 4 characters long.")
+    .optional(),
+  description: z
+    .string()
+    .min(10, "Description should be atleast 10 characters long")
+    .optional(),
+  price: z.number().positive("Price can't be negative").optional(),
+});
+
 export type CreateCourseInputType = z.infer<typeof createCourseInputSchema>;
-export type PurchaseCourseInputType = z.infer<typeof purchaseCourseInputSchema>;
+export type CourseIdInputType = z.infer<typeof courseIdInputSchema>;
+export type UpdateCourseInputType = z.infer<typeof updateCourseInputSchema>;

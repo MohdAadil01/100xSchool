0ac8eecd192f28c19c113912c63580f7c0593d65
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-24 16:47:53 +0530

    add course routes

--- webdev/day3/assignments/02_CourseSellingApi/src/routes/course.route.ts
+++ webdev/day3/assignments/02_CourseSellingApi/src/routes/course.route.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { createCourse } from "../controller/course.controller";
+import {
+  createCourse,
+  deleteCourse,
+  getCourseByid,
+  getCourses,
+  updateCourse,
+} from "../controller/course.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const courseRoute = Router();
 
 courseRoute.post("/", authMiddleware, createCourse);
+courseRoute.get("/", getCourses);
+courseRoute.get("/:courseId", getCourseByid);
+courseRoute.patch("/:courseId", authMiddleware, updateCourse);
+courseRoute.delete("/:courseId", authMiddleware, deleteCourse);
 
 export default courseRoute;

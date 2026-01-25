a77ef760366ab4ff48eb39de95432a1fe4f1ee1a
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-25 17:34:39 +0530

    refine create purchase

--- webdev/day3/assignments/02_CourseSellingApi/src/services/purchase.service.ts
+++ webdev/day3/assignments/02_CourseSellingApi/src/services/purchase.service.ts
@@ -7,16 +7,36 @@ export const createPurchaseService = async (
   courseId: string,
 ) => {
   if (role.toLowerCase() != "student") throw new AppError("Unauthorized", 403);
-  let purchase;
-  await prisma.$transaction(async (tx) => {
-    purchase = await tx.purchase.create({
+
+  return await prisma.$transaction(async (tx) => {
+    const course = await tx.course.findUnique({
+      where: {
+        id: courseId,
+      },
+    });
+    if (!course) throw new AppError("Course not found.", 404);
+
+    if (course.instructorId == userId) throw new AppError("Unauthorized", 403);
+
+    const existingPurchase = await tx.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId,
+          courseId,
+        },
+      },
+    });
+    if (existingPurchase) throw new AppError("Course already purchased", 409);
+
+    const purchase = await tx.purchase.create({
       data: {
         userId,
         courseId,
       },
     });
+
+    return purchase;
   });
-  return purchase;
 };
 
 export const getUserPurchasesService = async (userId: string) => {

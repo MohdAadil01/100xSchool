594f7af67b6216416a2463a135cf2ad889d2b6f7
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-27 21:28:45 +0530

    add validation for auth

---
+++ webdev/assignments/03_HotelBookingPlatformApi/src/validation/auth.validation.ts
@@ -0,0 +1,23 @@
+import z, { email } from "zod";
+
+export const signupInputSchema = z.object({
+  name: z.string().min(1, "Name too short"),
+  email: z.string().email("Invalid Email"),
+  password: z.string().min(6, "Password too short"),
+  role: z
+    .string()
+    .transform((val) => val.toUpperCase())
+    .pipe(z.enum(["OWNER", "CUSTOMER"])),
+  phone: z.string().regex(/^\+91[6-9]\d{9}$/, {
+    message: "Invalid Indian phone number (+91XXXXXXXXXX required)",
+  }),
+});
+
+export const loginInputSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Password too short"),
+});
+
+export type SignupInputType = z.infer<typeof signupInputSchema>;
+
+export type LoginInputType = z.infer<typeof loginInputSchema>;

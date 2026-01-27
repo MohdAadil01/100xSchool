1833085ae310bc969a8912ac263bf2c96c955bbf
MohdAadil01 <mohdaadil4240@gmail.com>
2026-01-27 22:03:55 +0530

    add auth route

--- webdev/assignments/03_HotelBookingPlatformApi/src/app.ts
+++ webdev/assignments/03_HotelBookingPlatformApi/src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
 import bodyParser from "body-parser";
+import { globalErrorHandler } from "./middleware/globalError.middleware";
+import authRoute from "./routes/auth.route";
 
 const app = express();
 config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// !using routes
+app.use("/api/auth", authRoute);
+
+// !using middlewares
+app.use(globalErrorHandler);
 
 export default app;

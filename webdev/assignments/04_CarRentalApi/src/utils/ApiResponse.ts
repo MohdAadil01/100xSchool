f12063aaa287b3c8321f203d39d9c6635e69102a
MohdAadil01 <mohdaadil4240@gmail.com>
2026-02-03 16:40:53 +0530

    add api response

---
+++ webdev/assignments/04_CarRentalApi/src/utils/ApiResponse.ts
@@ -0,0 +1,16 @@
+export class ApiResponse<T = any> {
+  success: boolean;
+  data: T | null;
+  error: string | null;
+
+  constructor(success: boolean, data: T, error: string | null) {
+    ((this.success = success), (this.data = data), (this.error = error));
+  }
+
+  static success<T>(data: T) {
+    return new ApiResponse(true, data, null);
+  }
+  static error(message: string) {
+    return new ApiResponse(false, null, message);
+  }
+}

64c3aa19fe337decf2b9fd6f178067e1205514cb
MohdAadil01 <mohdaadil4240@gmail.com>
2026-02-04 14:38:57 +0530

    add auth routes

--- webdev/assignments/04_CarRentalApi/src/app.ts
+++ webdev/assignments/04_CarRentalApi/src/app.ts
@@ -1,9 +1,12 @@
 import bodyParser from "body-parser";
 import express from "express";
+import authRoute from "./routes/auth.route";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", authRoute);
 
 export default app;

627d321a87c7facd59ebb2fb80ab5fb955a056e6
MohdAadil01 <mohdaadil4240@gmail.com>
2026-02-04 14:37:03 +0530

    add auth controller

---
+++ webdev/assignments/04_CarRentalApi/src/controllers/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { AsyncHandler } from "../utils/AsyncHandler";
+import { authInputSchema } from "../validators/auth.validator";
+import { loginService, signupService } from "../services/auth.services";
+import { ApiResponse } from "../utils/ApiResponse";
+
+export const signup = AsyncHandler(async (req: Request, res: Response) => {
+  const parsedBody = authInputSchema.parse(req.body);
+  const data = await signupService(parsedBody);
+
+  return res.status(201).json(ApiResponse.success(data));
+});
+export const login = AsyncHandler(async (req: Request, res: Response) => {
+  const parsedBody = authInputSchema.parse(req.body);
+  const data = await loginService(parsedBody);
+
+  return res.status(201).json(ApiResponse.success(data));
+});

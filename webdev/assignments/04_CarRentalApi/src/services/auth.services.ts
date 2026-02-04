538b41886631398f857ab9e5cf4eac904917bdcd
MohdAadil01 <mohdaadil4240@gmail.com>
2026-02-04 12:52:22 +0530

    add auth services

--- webdev/assignments/04_CarRentalApi/src/services/auth.services.ts
+++ webdev/assignments/04_CarRentalApi/src/services/auth.services.ts
@@ -4,7 +4,7 @@ import { prisma } from "../lib/db";
 import { AppError } from "../utils/Error";
 import { generateToken } from "../utils/jwt";
 
-export const signup = async (input: AuthInputType) => {
+export const signupService = async (input: AuthInputType) => {
   const { username, password } = input;
   const existingUser = await prisma.user.findFirst({
     where: {
@@ -13,13 +13,21 @@ export const signup = async (input: AuthInputType) => {
   });
   if (existingUser) throw new AppError(409, "username already exists");
 
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(process.env.SALT_ROUNDS!),
+  );
+
   const user = await prisma.user.create({
     data: {
       username,
-      password,
+      password: hashedPassword,
     },
   });
-  return user;
+  return {
+    message: "User created successfully",
+    userId: user.id,
+  };
 };
 
 export const loginService = async (input: AuthInputType) => {
@@ -38,4 +46,9 @@ export const loginService = async (input: AuthInputType) => {
   if (!isPasswordCorrect) throw new AppError(401, "incorrect password");
 
   const token = generateToken({ id: existingUser.id, username });
+
+  return {
+    message: "Login successful",
+    token: token,
+  };
 };

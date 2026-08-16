fb7e6f03252f160d542ea100632111031e5f0925
MohdAadil01 <mohdaadil4240@gmail.com>
2026-08-16 19:33:11 +0530

    add localhost origin

--- webdev/assignments/09_HotelManagement/src/app.ts
+++ webdev/assignments/09_HotelManagement/src/app.ts
@@ -18,7 +18,7 @@ export const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ENV.CLIENT_URL || "http://localhost:5173",
+    origin: [ENV.CLIENT_URL, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),

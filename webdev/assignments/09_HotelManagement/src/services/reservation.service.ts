e368445a0e0e898ddd4b09aa09cf9c480be4c2ed
MohdAadil01 <mohdaadil4240@gmail.com>
2026-08-16 20:33:24 +0530

    add search input func

--- webdev/assignments/09_HotelManagement/src/services/reservation.service.ts
+++ webdev/assignments/09_HotelManagement/src/services/reservation.service.ts
@@ -170,8 +170,6 @@ const create = async (input: CreateReservationInputType, createdBy: string) => {
     specialRequests,
   });
 
-  console.log(guestDetails);
-
   try {
     if (guestDetails?.email) {
       await sendMail(

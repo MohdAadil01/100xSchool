5df5b19b0ee7bda78b760eb37f8f71d5693a4722
MohdAadil01 <mohdaadil4240@gmail.com>
2026-08-31 17:25:18 +0530

    add auth route

---
+++ webdev/assignments/10_HospitalManagementSystem/backend/src/routes/auth.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { authController } from "../controller/auth.controller";
+
+export const authRouter = Router();
+
+authRouter.post("/patient/register", authController.registerPatient);
+
+authRouter.post("/staff/register", authController.registerStaff);
+
+authRouter.post("/login", authController.login);

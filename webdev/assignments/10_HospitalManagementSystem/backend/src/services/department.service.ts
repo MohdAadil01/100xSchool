f46b43dae8608730df0b04f5949516dadf696c59
MohdAadil01 <mohdaadil4240@gmail.com>
2026-09-01 23:53:09 +0530

    add department service:

---
+++ webdev/assignments/10_HospitalManagementSystem/backend/src/services/department.service.ts
@@ -0,0 +1,75 @@
+import { Department } from "../models/Department.model";
+import { Hospital } from "../models/Hospital.model";
+import { AppError } from "../utils/AppError";
+import {
+  CreateDepartmentInputType,
+  UpdateDepartmentInputType,
+} from "../validators/department.validator";
+
+const create = async (input: CreateDepartmentInputType) => {
+  const { name, description, hospital } = input;
+  const hospitalExists = await Hospital.findById(hospital);
+  if (!hospitalExists) throw new AppError(404, "Hospital not found.");
+
+  const departmentExists = await Department.findOne({ name, hospital });
+  if (departmentExists)
+    throw new AppError(400, "Department already exists in this Hospital.");
+
+  const department = await Department.create({
+    name,
+    description,
+    hospital,
+  });
+
+  return department.toObject();
+};
+
+const getAll = async (hospital: string, query?: { name?: string }) => {
+  if (!hospital) throw new AppError(404, "Hospital ID not found");
+
+  const hospitalExists = await Hospital.findById(hospital);
+  if (!hospitalExists) throw new AppError(404, "Hospital not found.");
+
+  const departments = await Department.find({
+    hospital,
+    name: { $regex: `^${query?.name}`, $options: "i" },
+  });
+
+  return departments;
+};
+
+const getById = async (departmentId: string) => {
+  if (!departmentId) throw new AppError(404, "Department ID not found.");
+
+  const department = await Department.findById(departmentId);
+
+  if (!department) throw new AppError(404, "Department not found.");
+
+  return department.toObject();
+};
+
+const update = async (
+  input: UpdateDepartmentInputType,
+  hospital: string,
+  departmentId: string,
+) => {
+  const { name, description } = input;
+
+  const department = await Department.findOneAndUpdate(
+    {
+      _id: departmentId,
+      hospital,
+    },
+    { name, description },
+  );
+  if (!department) throw new AppError(404, "Department not found.");
+
+  return department.toObject();
+};
+
+export const departmentService = {
+  create,
+  getAll,
+  getById,
+  update,
+};
